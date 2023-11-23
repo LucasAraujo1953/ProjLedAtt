4fb099908de9bc0a66f0d2608631ca3976826c66
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-11-23 19:45:03 -0300

    Add files via upload

---
+++ projLeds/projLeds/Paralela.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace projLeds
+{
+    internal class Paralela
+    {
+        // Escreve um byte no endereço
+        [DllImport("Inpout32.dll", EntryPoint = "Out32")]
+        public static extern void Escrever(int endereco, byte valor);
+
+        // Lê um byte do endereço
+        [DllImport("Inpout32.dll", EntryPoint = "Inp32")]
+        public static extern byte Ler(int endereco);
+    }
+}
